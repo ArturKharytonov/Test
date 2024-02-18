5ca6dd74f39b21604deaf0a9b1e9d2bd5f4491b1
Artur <122572472+ArturKharytonov@users.noreply.github.com>
2024-02-18 12:08:56 +0200

    Revert "test commit to test2"
    This reverts commit afe13f2b54f768422c2042547e2167e510cdb47f.

--- TestTask/TestTask/Program.cs
+++ TestTask/TestTask/Program.cs
@@ -7,25 +7,11 @@ namespace TestTask
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine($"Total count of ships - {Game.GetCountOfShips()}");
-            //Console.WriteLine($"One deck - {Game.KindOfShips[0]}\n" +
-            //                  $"Two decks - {Game.KindOfShips[1]}\n" +
-            //                  $"Three decks - {Game.KindOfShips[2]}\n" +
-            //                  $"Four decks - {Game.KindOfShips[3]}");
-
-            for (int i = 0; i < 100; i++)
-            {
-                Console.Write(i);
-
-                if(i % 3 == 0 && i % 5 == 0)
-                    Console.Write(" FizzBuzz");
-                else if(i % 3 == 0)
-                    Console.Write(" Fizz");
-                else if(i % 5 == 0)
-                    Console.Write(" Buzz");
-
-                Console.WriteLine();
-            }
+            Console.WriteLine($"Total count of ships - {Game.GetCountOfShips()}");
+            Console.WriteLine($"One deck - {Game.KindOfShips[0]}\n" +
+                              $"Two decks - {Game.KindOfShips[1]}\n" +
+                              $"Three decks - {Game.KindOfShips[2]}\n" +
+                              $"Four decks - {Game.KindOfShips[3]}");
         }
     }
 }
